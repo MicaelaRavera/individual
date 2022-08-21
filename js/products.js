de0084969f0242c71412aa3c0a59be9267bf1310
Micaela Ravera <ayeleniparraguire@gmail.com>
2022-08-21 15:24:44 -0300

    mejoras visuales a la lista de productos

--- js/products.js
+++ js/products.js
@@ -1,11 +1,39 @@
 function mostrarProductos (lista)
 {
 	let productosdiv =document.getElementById ("productos")
+	productosdiv.innerHTML =`
+	<center class="titulo">
+		<h1>Productos</h1>
+		<span>Veras aqui todos los productos de la categoria
+		${lista.catName}
+		</span>
+	</center>
+	<hr>
+	`
+
 	for(let producto of lista.products){
 		productosdiv.innerHTML +=`
-		<h1>${producto.name} ${producto.currency} ${producto.cost}</h1>
-     	<img src="${producto.image}" width="450" height="300">
-     	<p>${producto.description}</p>
+
+	<div class="producto">
+     	<img src="${producto.image}"
+		 width="200"
+		 height="100">
+		 <div class="detalles">
+
+		 <h4>
+		 	${producto.name}
+			${producto.currency}
+			${producto.cost}
+		</h4>
+
+		${producto.description}
+		</div>
+	<span>
+		${producto.soldCount} vendidos
+	</span>
+
+	</div>
+	<hr>
 		`
 	}
 }
