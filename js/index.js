f0d6bc4bce101e4d2c9ecce10a1927b4d8ac5a63
Micaela Ravera <ayeleniparraguire@gmail.com>
2022-08-22 10:28:58 -0300

    actualizacion

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
-document.addEventListener("DOMContentLoaded",function(){
-	document.getElementById("boton").addEventListener("click",function(){
-
-		let inputemail = document.getElementById ("email")
-		let inputpassword = document.getElementById("password")
-		if (inputemail.value !== undefined && inputemail.value !== "" && inputpassword.value !== undefined && inputpassword.value !== "") {
-			window.location.replace("file:///home/fpalacios/Desktop/PROYECTO%20O/PROYECTO%20DE%20%20JaP/workspace-inicial/index.html")
-		}else{
-			alert("Error en el usuario y/o contraseña")
-
-		}
-	})
-})
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("autos").addEventListener("click", function() {
+        localStorage.setItem("catID", 101);
+        window.location = "products.html"
+    });
+    document.getElementById("juguetes").addEventListener("click", function() {
+        localStorage.setItem("catID", 102);
+        window.location = "products.html"
+    });
+    document.getElementById("muebles").addEventListener("click", function() {
+        localStorage.setItem("catID", 103);
+        window.location = "products.html"
+    });
+});
---
+++ js/login.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded",function(){
+	document.getElementById("boton").addEventListener("click",function(){
+
+		let inputemail = document.getElementById ("email")
+		let inputpassword = document.getElementById("password")
+		if (inputemail.value !== undefined && inputemail.value !== "" && inputpassword.value !== undefined && inputpassword.value !== "") {
+			window.location.replace("file:///home/fpalacios/Desktop/PROYECTO%20O/PROYECTO%20DE%20%20JaP/workspace-inicial/home.html")
+		}else{
+			alert("Error en el usuario y/o contraseña")
+
+		}
+	})
+})
