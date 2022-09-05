d4f365330360dec786e95086f78bbaa9297fe10b
Micaela Ravera <ayeleniparraguire@gmail.com>
2022-09-04 23:28:34 -0300

    Permitir que se ordenen y filtren productos

--- js/products.js
+++ js/products.js
@@ -1,39 +1,56 @@
 let precio = false
 let ascendente = false
+let filtroMinimo = -1
+let filtroMaximo = -1
 
 
-function mostrarProductos (lista)
-{
-	lista.products.sort((a,b) => {
-		let primero;
-		let segundo;
-		let orden;
-		if (ascendente === true){
-			orden = 1
-		}
-		else {
-			orden = -1
-		}
-		if (precio){
-			primero = a.cost
-			segundo = b.cost
-		}
-		else {
-			primero = a.soldCount 
-			segundo = b.soldCount
-		}
-		if (primero > segundo ){
-			return orden
-		}
-		else if (primero === segundo) {
-			return 0
-		}
-		else {
-			 return - orden
-		}
-	})
-	let productosdiv =document.getElementById ("productos")
-	productosdiv.innerHTML =`
+function mostrarProductos(lista) {
+	productsFiltrados = lista.products
+		.filter((product) => {
+			if (filtroMinimo >= 0) {
+				return product.cost > filtroMinimo;
+			} else {
+				return true;
+			}
+		})
+		.filter((product) => {
+			if (filtroMaximo >= 0) {
+				return product.cost < filtroMaximo;
+			} else {
+				return true;
+			}
+		})
+		.sort((a, b) => {
+			let primero;
+			let segundo;
+			let orden;
+			if (ascendente === true) {
+				orden = 1
+			}
+			else {
+				orden = -1
+			}
+			if (precio) {
+				primero = a.cost
+				segundo = b.cost
+			}
+			else {
+				primero = a.soldCount
+				segundo = b.soldCount
+			}
+			if (primero > segundo) {
+				return orden
+			}
+			else if (primero === segundo) {
+				return 0
+			}
+			else {
+				return -orden
+			}
+		})
+
+	let productosdiv = document.getElementById("productos")
+	productosdiv.innerHTML = `
 	<center class="titulo">
 		<h1>Productos</h1>
 		<span>Veras aqui todos los productos de la categoria
@@ -43,8 +60,8 @@ function mostrarProductos (lista)
 	<hr>
 	`
 
-	for(let producto of lista.products){
-		productosdiv.innerHTML +=`
+	for (let producto of productsFiltrados) {
+		productosdiv.innerHTML += `
 
 	<div class="producto">
      	<img src="${producto.image}"
@@ -70,5 +87,29 @@ function mostrarProductos (lista)
 	}
 }
 
-getJSONData(PRODUCTS_URL+localStorage.getItem("catID")+EXT_TYPE)
-	.then(productos => mostrarProductos(productos.data) )
+function cargarProductos() {
+	getJSONData(PRODUCTS_URL + localStorage.getItem("catID") + EXT_TYPE)
+		.then(productos => mostrarProductos(productos.data))
+}
+
+cargarProductos()
+document.getElementById("sortAscPrice").addEventListener("click", () => {
+	precio = true
+	ascendente = true
+	cargarProductos()
+})
+document.getElementById("sortDescPrice").addEventListener("click", () => {
+	precio = true
+	ascendente = false
+	cargarProductos()
+})
+document.getElementById("sortDescSoldCount").addEventListener("click", () => {
+	precio = false
+	ascendente = false
+	cargarProductos()
+})
+document.getElementById("filterPrices").addEventListener("click", () => {
+	filtroMinimo = document.getElementById("filterMinPrice").value ? document.getElementById("filterMinPrice").value : -1
+	filtroMaximo = document.getElementById("filterMaxPrice").value ? document.getElementById("filterMaxPrice").value : -1
+	cargarProductos()
+})
