dc69c4bfc24063a8b678be94bdbb18797e9f5aad
micaela ravera <ayeleniparraguire@gmail.com>
2022-10-15 00:46:49 -0300

    se hizo la modificacion del subtotal

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,8 @@
 function cargarCarrito() {
-    getJSONData(CART_INFO_URL + 25801 + EXT_TYPE)
+      getJSONData(CART_INFO_URL + 25801 + EXT_TYPE)
         .then(productos => mostrarCarrito(productos.data))
+    
+    
 }
 
 cargarCarrito()
@@ -8,12 +10,27 @@ cargarCarrito()
 function mostrarCarrito(jsonlista){
     let detallescarrito = document.getElementById("carrito")
     for (let producto of jsonlista.articles)
-    detallescarrito.innerHTML = `
+    detallescarrito.innerHTML += `
     <tr>
         <th scope="row"> <img class="carrito" src="${producto.image}"> ${producto.name}</th>
         <td> ${producto.currency} ${producto.unitCost}</td>
-        <td> <input class="form-control carritoCantidad" type="number"  value=${producto.count}></td>
-        <td>${producto.currency} ${producto.unitCost*producto.count}</td>
+        <td> <input  oninput="subtotal(event.target.value,${producto.unitCost},${producto.id})" class="form-control carritoCantidad" type="number"  value=${producto.count}></td>
+        <td>${producto.currency} 
+        <span  id="subtotal${producto.id}">
+         ${producto.unitCost*producto.count}
+         </span>
+         </td>
     </tr>
     `
 }
+
+function subtotal(cantidad, cost,id) {
+    let subtotalspan = document.getElementById("subtotal"+id)
+    subtotalspan.innerHTML = `
+    ${cantidad * cost }
+    `
+ 
+}
+
+
+
